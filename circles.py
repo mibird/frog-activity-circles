77988d7350eac88cd2e0a6bc255a4219d80ce05f
mibird <mibirdtheteam@gmail.com>
2026-02-13 22:13:27 +0000

    Annotation

--- circles.py
+++ circles.py
@@ -1,6 +1,7 @@
-
 pi = float(3.14159265359)
 isloop = True
+
+# Gets the angle of the arc from the user
 def angleIn():
     angle = input("Arc angle \33[1;33m(0 for whole)\33[0m:\n")
     angle = float(angle)
@@ -8,18 +9,19 @@ def angleIn():
         angle = 360
     return angle
 
+# Calculates the circumference of a circle or arc
 def circumferenceCalculation(israd,measurement):
     angle = angleIn()
 
     if israd:
         measurement *= 2
         ans = (measurement * pi)*(angle/360)
-
     else:
         ans = (measurement * pi)*(angle/360)
 
     return ans
 
+# Calculates the area of a circle or arc
 def areaCalculation(israd,measurement):
     angle = angleIn()
 
@@ -34,6 +36,7 @@ def areaCalculation(israd,measurement):
 
     return ans
 
+# Calculates the volume of a cylinder, with the addition of a request of the length of the cylinder
 def volumeCalculation(israd,measurement):
     length = float(input("length of cylinder: "))
 
@@ -48,6 +51,7 @@ def volumeCalculation(israd,measurement):
 
     return ans
 
+# Asks for either a radius or perimeter
 def Radius_or_Perimeter_Meaasurements():
     selection = []
     ans = input("\33[1;3;4m///Measurement///\33[0m\nRadius \033[1;33m(r)\033[0m \nPerimeter \033[1;33m(p)\033[0m\n")
@@ -65,6 +69,7 @@ def Radius_or_Perimeter_Meaasurements():
     selection.append(measurement)
     return selection
 
+# Asks for a selection between calculating the Circumference, Area or Volume
 def circumference_Area_Volume_Calculation(selection:list) -> list | int:
     if type(selection) == int:
         print("Please enter either radius or perimeter")
@@ -94,6 +99,7 @@ def circumference_Area_Volume_Calculation(selection:list) -> list | int:
     print("Result is:")
     print(out)
 
+# Code execution
 while isloop:
 
     radius_or_Perimeter = Radius_or_Perimeter_Meaasurements()
